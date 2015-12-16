2c758b56540120c937654a6deb77dafc0b3ade21
onlyxxp <onlyxxp@gmail.com>
2015-12-17 02:42:15 +0800

    add proxy ip support

--- src/multi_thread_verify.py
+++ src/multi_thread_verify.py
@@ -18,15 +18,30 @@ user_agents = [
     "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.1)",
 ]
 
+proxys = [
+    "124.206.133.227:80",
+    "118.244.151.157:3128",
+    # "114.41.143.186:3128",
+    # "221.228.248.244:8085",
+    # "101.81.67.13:9797",
+    # "116.1.233.53:8080",
+    # "58.67.159.50:80",
+    # "183.5.199.62:9797",
+    # "183.135.152.61:9999",
+    # "218.75.26.44:808"
+]
+
 success_result = []
 
 class GetUrlThread(Thread):
-    def __init__(self, word, ip):
+    def __init__(self, word):
         self.code = word
-        self.ip = ip
         super(GetUrlThread, self).__init__()
 
     def run(self):
+        proxy_index = random.randint(0, len(proxys) - 1)
+        ip = proxys[proxy_index]
+
         value = {'reginvcode': self.code, "action": 'reginvcodeck'}
         value_encoded = urllib.urlencode(value)
         url = 'http://clsq.co/register.php'
@@ -34,11 +49,16 @@ class GetUrlThread(Thread):
         user_agent_ = user_agents[random.randint(0, len(user_agents) - 1)]
         headers = {
             'User-Agent': user_agent_,
-            "X-Forwarded-for": self.ip
+            # "X-Forwarded-for": ip
         }
 
-        req = urllib2.Request(url, data=value_encoded, headers=headers)
+        print(ip + " " + user_agent_ + "  -->")
 
+        proxy = urllib2.ProxyHandler({'http': ip})
+        opener = urllib2.build_opener(proxy)
+        urllib2.install_opener(opener)
+
+        req = urllib2.Request(url, data=value_encoded, headers=headers)
         resp = urllib2.urlopen(req, timeout=10)
 
         # retry
@@ -53,7 +73,7 @@ class GetUrlThread(Thread):
             resp = urllib2.Request(url, data=value_encoded, headers=headers)
 
         resp_read = resp.read()
-        print(self.ip + " " + user_agent_ + " response: " + resp_read)
+        print("     response: " + resp_read)
         if len(str(resp_read).strip()) > 0:
             success = str(resp_read).find("parent.retmsg_invcode('1');") < 0
         else:
@@ -71,10 +91,10 @@ def get_responses(code_list):
     default_ip = "21.23.44."
 
     for code in list(code_list):
-        t = GetUrlThread(code, default_ip + str(i))
+        t = GetUrlThread(code)
         threads.append(t)
         t.start()
-        time.sleep(0.5)
+        time.sleep(1)
         i += 1
     for t in threads:
         t.join()
