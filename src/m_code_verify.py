13d218ad869cdbdd0de45e82c219f461e0c816df
工口 <xinpeng.xxp@alibaba-inc.com>
2015-12-18 21:23:06 +0800

    fix bugs

--- src/m_code_verify.py
+++ src/m_code_verify.py
@@ -10,18 +10,18 @@ from src import m_ip
 from threading import Thread
 
 user_agents = [
-    "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0) ",
-    "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.2) ",
-    "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1) ",
-    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0) ",
-    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.2) ",
-    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1) ",
-    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 6.0)",
-    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2)",
-    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)",
-    "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 6.0)",
-    "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.2)",
-    "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.1)",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36",
+    # "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.2) ",
+    # "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1) ",
+    # "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0) ",
+    # "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.2) ",
+    # "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1) ",
+    # "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 6.0)",
+    # "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2)",
+    # "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)",
+    # "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 6.0)",
+    # "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.2)",
+    # "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.1)",
 ]
 
 max_retry_num = 6
@@ -34,7 +34,13 @@ success = False
 
 mutex = threading.Lock()
 current_thread_count = 0
-max_thread_count = max(m_ip.get_ip_size(), 1)
+
+
+def update_max_thread_count():
+    global max_thread_count, mutex
+    mutex.acquire(100)
+    max_thread_count = max(m_ip.get_ip_size(), 1)
+    mutex.release()
 
 
 class GetUrlThread(Thread):
@@ -47,7 +53,7 @@ class GetUrlThread(Thread):
 
     @staticmethod
     def thread_count_increase():
-        global current_thread_count
+        global mutex, current_thread_count
         mutex.acquire(100)
         current_thread_count += 1
         mutex.release()
@@ -85,51 +91,65 @@ class GetUrlThread(Thread):
         req = urllib2.Request(url, data=value_encoded, headers=headers)
         return req, url
 
-    def transact_http_result(self, resp, url):
+    def transact_http_result(self, resp, url, start):
         global success, success_result, bad_ip
         resp_read = str(resp)
         if len(str(resp_read).strip()) > 0:
             success = str(resp_read).find(r"retmsg_invcode('0');") > 0
-            print self.index, "<==", self.ip_with_port, success, self.code, "   response: " + resp_read
+            print self.index, "<==", self.ip_with_port, success, self.code, " time:", time.time() - start, "   response: " + resp_read
         else:
             success = False
             self.append_to_bad_ip(self.ip_with_port)
-            print self.index, "<==", self.ip_with_port, success, self.code, "   response is EMPTY ", self.ip_with_port
+            print self.index, "<==", self.ip_with_port, success, self.code, " time:", time.time() - start, "   response is EMPTY ", self.ip_with_port
         if success:
             success_result.append(self.code)
 
-    def append_to_bad_ip(self, ip_with_port):
+    @staticmethod
+    def append_to_bad_ip(ip_with_port):
         global bad_ip
         m_ip.remove_bad_ip(ip_with_port)
         if ip_with_port not in bad_ip:
             bad_ip.append(ip_with_port)
+        update_max_thread_count()
 
     def run(self):
         self.thread_count_increase()
+        self.verify()
+        self.thread_count_decrease()
+
+    def verify(self):
         global max_retry_num, http_failed_codes, bad_ip, success_result, success
         req, url = self.build_request()
         for i in range(max_retry_num):
             if success:
                 print "run is skip because success"
                 break
+            start = time.time()
             try:
-                resp = urllib2.urlopen(req, timeout=5).read()
-                self.transact_http_result(resp, url)
+                resp = urllib2.urlopen(req, timeout=15).read()
+                self.transact_http_result(resp, url, start)
                 break
             except:
                 if i < max_retry_num - 1:
-                    print 'URLError: retry later ...', self.ip_with_port
+                    print 'URLError:', self.ip_with_port, "   ", i, 'th retry later ...'
                     time.sleep(2)
                     continue
                 else:
                     self.append_to_bad_ip(self.ip_with_port)
                     http_failed_codes.append(self.code)
                     print 'URLError: <urlopen error timed out> All times is failed ', self.ip_with_port
-        self.thread_count_decrease()
+
+
+def get_speed():
+    ip_size = m_ip.get_ip_size()
+    if ip_size == 0:
+        return 1.0
+    return 1.0/ip_size
 
 
 def get_responses(code_list, url):
     global success, success_result, http_failed_codes, bad_ip, current_thread_count, max_thread_count
+    update_max_thread_count()
     start = time.time()
     threads = []
 
@@ -146,8 +166,8 @@ def get_responses(code_list, url):
             print 'skip because success'
             break
         while current_thread_count >= max_thread_count:
-            speed = m_ip.get_speed()
-            print 'sleep ', speed, 'to avoid to much threads, current threads count is ', current_thread_count
+            speed = get_speed()
+            # print '   zZZzZZ sleep ', speed, 's, threads count is ', current_thread_count
             time.sleep(speed)
 
     for t in threads:
--- src/m_ip.py
+++ src/m_ip.py
@@ -3,14 +3,17 @@ __author__ = 'xxp'
 import random
 
 proxy_list = [
-    "124.206.133.227:80",
-    "221.176.14.72:80",
-    "120.195.201.16:80",
-    "120.195.201.196:80",
-    "117.136.234.8:80",
-    "218.97.194.223:80",
-    "124.88.67.22:80",
-    "218.97.194.222:80",
+    # "127.0.0.1:16808",
+    # "120.52.73.39:8080",
+    # "120.52.73.37:8080",
+    # "124.88.67.22:81",
+    # "187.254.216.157:8080",
+    # "222.39.112.12:8118",
+    # "113.255.129.42:80",
+    # "117.136.234.8:80",
+    # "218.97.194.223:80",
+    # "222.45.85.210:8118",
+    # "218.97.194.222:80",
 ]
 
 def get_ip_with_port():
@@ -30,14 +33,6 @@ def remove_bad_ip(ip_with_port):
     print "remove bad ip XXXXX ", ip_with_port
     if ip_with_port in proxy_list:
         proxy_list.remove(ip_with_port)
-
-
-def get_speed():
-    global proxy_list
-    ip_size = len(proxy_list)
-    if ip_size == 0:
-        return 2
-    return 2/float(ip_size)
 
 
 def get_ip_size():
