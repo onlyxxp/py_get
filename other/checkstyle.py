191b297305e75a398c590647dc73f968cdfb1507
工口 <xinpeng.xxp@alibaba-inc.com>
2015-12-19 20:00:23 +0800

    add checkstyle

---
+++ other/checkstyle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+
+def count_error(file_name):
+    line_counts = 0
+    error_counts = 0
+    character_counts = 0
+
+    with open(file_name, 'r') as f:
+        for line in f:
+            if line.find(r"<error line"):
+                error_counts += 1
+            line_counts += 1
+            character_counts += len(line)
+    return error_counts
+
+
+def checkstyle_result_path(project_path):
+    error_count = 0
+    if os.path.isdir(project_path):
+        for d in os.listdir(project_path):
+            check_file = project_path + '\\' + d + r"\target\checkstyle-result.xml"
+            if os.path.exists(check_file):
+                file_error = count_error(check_file)
+                error_count += file_error
+                print check_file, file_error
+
+    print "==============="
+    print project_path, error_count
+
+
+# count_error(r"D:\codes\TTEntertainment\Music\target\checkstyle-result.xml")
+
+checkstyle_result_path(r"D:\codes\TTFansClub")
+
