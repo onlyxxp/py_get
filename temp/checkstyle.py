8ca0682785cc96e81ecdd59e90486abcd8e2deda
工口 <xinpeng.xxp@alibaba-inc.com>
2015-12-21 20:48:52 +0800

    update python script

--- temp/checkstyle.py
+++ temp/checkstyle.py
@@ -10,7 +10,7 @@ def count_error(file_name):
 
     with open(file_name, 'r') as f:
         for line in f:
-            if line.find(r"<error line"):
+            if line.find(r"severity=") > 0:
                 error_counts += 1
             line_counts += 1
             character_counts += len(line)
@@ -19,6 +19,9 @@ def count_error(file_name):
 
 def checkstyle_result_path(project_path):
     error_count = 0
+    root_result_file = project_path + r"\target\checkstyle-result.xml"
+    error_count += count_error(root_result_file)
+    print root_result_file, error_count
     if os.path.isdir(project_path):
         for d in os.listdir(project_path):
             check_file = project_path + '\\' + d + r"\target\checkstyle-result.xml"
@@ -33,5 +36,5 @@ def checkstyle_result_path(project_path):
 
 # count_error(r"D:\codes\TTEntertainment\Music\target\checkstyle-result.xml")
 
-checkstyle_result_path(r"D:\codes\TTFansClub")
+checkstyle_result_path(r"D:\codes\Navigator-Android")
 
